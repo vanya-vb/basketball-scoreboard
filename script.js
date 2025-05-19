f219e67939fb6b8d0720e4dc1c88bfe6ad54de26
vanya-vb <vbozadzhieva29@gmail.com>
2025-05-19 14:38:43 +0300

    add functionality

---
+++ script.js
@@ -0,0 +1,51 @@
+const team1Container = document.getElementById('team-1');
+const team2Container = document.getElementById('team-2');
+
+const scoreContainerTeam1 = team1Container.querySelector('.score-container');
+const scoreContainerTeam2 = team2Container.querySelector('.score-container');
+
+const pointsContainerTeam1 = team1Container.querySelector('.points-container');
+const pointsContainerTeam2 = team2Container.querySelector('.points-container');
+
+const newGameBtn = document.getElementById('new-game-btn');
+
+let scoreTeam1 = 0;
+let scoreTeam2 = 0;
+
+pointsContainerTeam1.addEventListener('click', (e) => {
+    if (e.target.classList.contains("btn-1")) {
+        scoreTeam1++;
+        console.log(scoreContainerTeam1)
+    } else if (e.target.classList.contains("btn-2")) {
+        scoreTeam1 += 2;
+    } else if (e.target.classList.contains("btn-3")) {
+        scoreTeam1 += 3;
+    }
+
+    scoreContainerTeam1.textContent = scoreTeam1;
+    return scoreTeam1;
+});
+
+pointsContainerTeam2.addEventListener('click', (e) => {
+    if (e.target.classList.contains("btn-1")) {
+        scoreTeam2++;
+        console.log(scoreContainerTeam1)
+    } else if (e.target.classList.contains("btn-2")) {
+        scoreTeam2 += 2;
+    } else if (e.target.classList.contains("btn-3")) {
+        scoreTeam2 += 3;
+    }
+
+    scoreContainerTeam2.textContent = scoreTeam2;
+    return scoreTeam2;
+});
+
+newGameBtn.addEventListener('click', resetGame)
+
+function resetGame() {
+    scoreTeam1 = 0;
+    scoreContainerTeam1.textContent = scoreTeam1;
+
+    scoreTeam2 = 0;
+    scoreContainerTeam2.textContent = scoreTeam2;
+}
